1a59634d48f428cd040545f29dbba4e382c4da3c
Juan Camilo Herrera <juan8132juan@gmail.com>
2024-09-12 22:26:40 -0500

    Primmer Commit

---
+++ src/main/java/com/test/vehicleReservation/VehicleReservationApplication.java
@@ -0,0 +1,13 @@
+package com.test.vehicleReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/vehicleReservation/VehicleReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.vehicleReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
